78372b6dc903d56978b82d4e67b05ced2ccb7beb
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-08-15 16:25:43 -0400

    hash_set

--- hash_and_LRUCache/lib/p03_hash_set.rb
+++ hash_and_LRUCache/lib/p03_hash_set.rb
@@ -4,20 +4,44 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(key)
+
+    key = key.to_s if key.is_a?(Symbol)
+
+    return false if self.include?(key.hash)
+
+    @count += 1
+
+    resize! if @count >= num_buckets
+
+    @store[key.hash % @num_buckets] << key.hash
+
+
+    return true
   end
 
   def include?(key)
+    return self[key].include?(key.hash)
   end
 
   def remove(key)
+    return false if !include?(key)
+
+    @store[key.hash % @num_buckets].delete(key.hash)
+    @count -= 1
+
+
   end
 
   private
 
+
   def [](num)
+    @store[num.hash % num_buckets]
+
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +50,14 @@ class HashSet
   end
 
   def resize!
+    @num_buckets *= 2
+    resized = Array.new(@num_buckets) {[]}
+
+    @store.each do |bucket|
+      bucket.each {|hsh| resized[hsh % @num_buckets] = hsh}
+    end
+
+    @store = resized
+
   end
 end
